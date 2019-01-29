e4aaf0bfaad6081d7ae45e51c7b014f785920dc9
GiblerW <westongibler@gmail.com>
2019-01-29 13:28:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Billy's Page
           </p>
           <a
             className="App-link"
