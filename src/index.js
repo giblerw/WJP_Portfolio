67587a93c014d682f242b8ab935617c839e73f28
GiblerW <westongibler@gmail.com>
2019-01-29 17:41:46 -0700

    added some styling

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'mdbreact/dist/css/mdb.css';
+import 'mdbreact/dist/css/mdb.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import {BrowserRouter as Router} from 'react-router-dom';
-// import Home from './Component/Home';
-// import About from './Component/About';
+
 
 ReactDOM.render(
   <Router>
     <App />
   </Router> , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (module.hot) {
+      module.hot.accept();
+}
